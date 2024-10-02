3e0921375faa8644f8fa216036c7920f038e8538
Alex Zaytseff <alex.zaytseff@gmail.com>
2024-10-03 00:03:12 +0400

    Added save plugin options

--- inc/class-mccp-gateway.php
+++ inc/class-mccp-gateway.php
@@ -5,11 +5,12 @@ if (!defined('ABSPATH')) exit;
 use Apirone\SDK\Invoice;
 use Apirone\SDK\Model\Settings as Options;
 use Apirone\SDK\Model\Settings\Currency;
+use PhpParser\Node\Expr\Instanceof_;
 
 class WC_MCCP extends WC_Payment_Gateway
 {
-    public Options $options;
-
+    public ?Options $options;
+    
     public function __construct()
     {
         $this->id = 'mccp';
@@ -27,16 +28,43 @@ class WC_MCCP extends WC_Payment_Gateway
         add_action('woocommerce_update_options_payment_gateways_mccp', array($this, 'process_admin_options'));
         add_action('woocommerce_admin_order_data_after_billing_address', array($this, 'show_invoice_admin_info'));
     }
-
     public function init()
     {
+        $this->do_update();
         $this->init_settings();
-        // $this->do_update();
-        
-        // $this->options = Settings::fromJson(get_option('woocommerce_mccp_settings')['options']);
+        $this->get_options();
+    }
+    // Load existing og create new
+    public function get_options()
+
+    {
+        if(isset($this->options)) {
+            return $this->options;
+        }
+        $json = $this->get_option('options');
+        if ($json) {
+            $this->options = Options::fromJson($json);
+        }
+        else {
+            $this->options = Options::init()->createAccount();
+            $this->update_option('options', $this->options->toJson());
+        }
+
+        return $this->options;
     }
 
-    public function invoice_receipt($order_id) {
+    public function get_secret($renew = false) {
+        $secret = $this->get_option('secret');
+
+        if ( !$secret || $renew ) {
+            $secret = md5(time());
+            $this->update_option('secret', $secret);
+        }
+        return $secret;
+    }
+
+    public function invoice_receipt($order_id)
+    {
         echo(__FUNCTION__);
     }
 
@@ -46,13 +74,34 @@ class WC_MCCP extends WC_Payment_Gateway
         echo 'Coming soon';   
     }
 
+    public function get_option($key, $empty_value = null)
+    {
+        if(isset($this->options)) {        
+            $property = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $key))));
+
+            if (property_exists($this->options, $property)) {
+                $method = 'get' . ucfirst($property);
+                $value = $this->options->$method();
+                return is_bool($value) ? (($value) ? 'yes' : 'no') : $value;
+            }
+
+            $extra = $this->options->getExtra($key);
+            if ($extra) {
+                return $extra;
+            }
+        }
+
+        return parent::get_option($key, $empty_value);
+    }
+
     /**
      * Payment process handler
      * 
      * @param int $order_id 
      * @return array 
      */
-    public function process_payment($order_id) {
+    public function process_payment($order_id)
+    {
         $order = new WC_Order($order_id);
         // Create redirect URL
         $redirect = $order->get_checkout_payment_url(true);
@@ -69,15 +118,58 @@ class WC_MCCP extends WC_Payment_Gateway
 
     public function process_admin_options()
     {
-        $this->set_form_fields();
-        return parent::process_admin_options();
+        $this->form_fields = $this->admin_fields();
+        parent::process_admin_options();
+		
+        $post_data = $this->get_post_data();
+        $options = $this->options_fields();
 
+        $this->options->setMerchant($this->get_field_value('merchant', $options['merchant'], $post_data));
+        $this->options->setExtra('test_customer', $this->get_field_value('test_customer', $options['test_customer'], $post_data));
+        $this->options->setTimeout((int) $this->get_field_value('timeout', $options['timeout'], $post_data));
+        $this->options->setExtra('processing_fee', $this->get_field_value('processing_fee', $options['processing_fee'], $post_data));
+        $this->options->setFactor((float) $this->get_field_value('factor', $options['factor'], $post_data));
+        $this->options->setLogo($this->get_field_value('logo', $options['logo'], $post_data) == 'yes' ? true : false);
+        $this->options->setDebug($this->get_field_value('debug', $options['debug'], $post_data) == 'yes' ? true : false);
+
+        // set addresses & tokens
+        $networks_data = $this->get_field_value('networks', $options['networks'], $post_data);
+        $tokens_data = $this->get_field_value('tokens', $options['tokens'], $post_data);
+
+        foreach ($this->options->getNetworks() as $network) {
+
+            $this->options->getCurrency($network->abbr)->parseAbbr()->setAddress($networks_data[$network->abbr]);
+            $this->options->getCurrency($network->abbr)->setPolicy($this->options->getExtra('processing_fee'));
+            if ($network->isNetwork()) {
+                $tokens = $network->getTokens($this->options->currencies);
+                if($tokens) {
+                    $tokens = array_merge([$network], $tokens);
+                    foreach ($tokens as $token) {
+                        $this->options->getCurrency($token->abbr)->setPolicy($this->options->getExtra('processing_fee'));
+                        $this->options->getCurrency($token->abbr)->setAddress($network->getAddress());
+                        $this->options->setExtra($token->abbr, array_key_exists($token->abbr, $tokens_data) ? 1 : 0);
+                    }
+                }
+            }            
+        }
+        $this->options->saveCurrencies();
+        $this->update_option('options', $this->options->toJson());
     }
     
-    public function admin_options() {
+    public function admin_options()
+    {
         global $wp_version;
-        $this->set_form_fields();
-        $this->do_update();
+        $this->form_fields = $this->admin_fields();
+
+        foreach ($this->options->getNetworks() as $network) {
+            if ($network->hasError()) {
+                $this->add_error(sprintf("<strong>%s has error:</strong> %s", ...[$network->name, $network->getError()]));
+            }
+        }
+        if ($this->get_errors()) {
+            $this->display_errors();
+        }   
+
         ?>
             <h3><?php _e('Multi Crypto Currency Payment Gateway', 'mccp'); ?></h3>
             <div><?php _e('This plugin uses the Apirone crypto processing service.', 'mccp'); ?> <a href="https://apirone.com" target="_blank"><?php _e('Details'); ?></a></div>
@@ -87,8 +179,8 @@ class WC_MCCP extends WC_Payment_Gateway
             </table>
             <div><hr/>
             <h3>Plugin Info:</h3>
-                Version: <b><?php echo $this->get_option('version'); ?></b></br>
-                Account: <b><?php // echo $this->mccp_account()->account; ?></b><br/>
+                Version: <b><?php echo $this->get_option('version', 'n/a'); ?></b></br>
+                Account: <b><?php echo $this->options->getAccount(); ?></b><br/>
                 PHP version: <b><?php echo phpversion(); ?></b><br/>
                 WordPress: <b><?php echo $wp_version; ?></b><br/>
                 WooCommerce: <b><?php echo WC_VERSION; ?></b><br/>
@@ -98,8 +190,9 @@ class WC_MCCP extends WC_Payment_Gateway
         <?php
     }
 
-    public function set_form_fields() {
-        $this->form_fields = array(
+    public function admin_fields()
+    {
+        return array(
             'enabled' => array(
                 'title' => __('On/off', 'mccp'),
                 'type' => 'checkbox',
@@ -116,7 +209,8 @@ class WC_MCCP extends WC_Payment_Gateway
         );
     }
 
-    private function options_fields() {
+    private function options_fields()
+    {
         return array(
                 'merchant' => array(
                 'title' => __('Merchant name', 'mccp'),
@@ -160,7 +254,7 @@ class WC_MCCP extends WC_Payment_Gateway
                     'percentage' => __('Percentage'),
                     'fixed' => __('Fixed'),
                 ],
-                'default' => 'percentage',
+                'default' => 'fixed',
             ),
             'factor' => array(
                 'title' => __('Price adjustment factor', 'mccp'),
@@ -170,11 +264,13 @@ class WC_MCCP extends WC_Payment_Gateway
                 'desc_tip' => true,
                 'custom_attributes' => array('min' => 0.01, 'step' => '0.01', 'required' => 'required'),
             ),
-            'apirone_logo' => array(
+            'logo' => array(
                 'title' => __('Apirone logo', 'mccp'),
                 'type' => 'checkbox',
-                'label' => __('Show Apirone logo on invoice page', 'mccp'),
+                'label' => __('Display', 'mccp'),
                 'default' => 'yes',
+                'description' => __('Display Apirone logo on invoice page', 'mccp'),
+                'desc_tip' => true,
             ),
             'debug' => array(
                 'title' => __('Debug mode', 'mccp'),
@@ -187,7 +283,8 @@ class WC_MCCP extends WC_Payment_Gateway
         );
     }
 
-    public function generate_options_html ($key, $data) {
+    public function generate_options_html ($key, $data)
+    {
         $fields = $this->options_fields();
         ob_start();
         echo $this->generate_text_html('merchant', $fields['merchant']);
@@ -196,7 +293,7 @@ class WC_MCCP extends WC_Payment_Gateway
         echo $this->generate_networks_html('networks', $fields['networks']);
         echo $this->generate_select_html('processing_fee', $fields['processing_fee']);
         echo $this->generate_text_html('factor', $fields['factor']);
-        echo $this->generate_checkbox_html('apirone_logo', $fields['apirone_logo']);
+        echo $this->generate_checkbox_html('logo', $fields['logo']);
         echo $this->generate_checkbox_html('debug', $fields['debug']);
         return ob_get_clean();
     }
@@ -207,7 +304,8 @@ class WC_MCCP extends WC_Payment_Gateway
     * @param mixed $data 
     * @return string|false 
     */
-    public function generate_networks_html ($key, $data) {
+    public function generate_networks_html ($key, $data)
+    {
         ob_start();
         ?>
         <tr valign="top">
@@ -247,7 +345,10 @@ class WC_MCCP extends WC_Payment_Gateway
                                                 <span class="currency-icon <?php echo str_replace('@', '_', esc_html($token->getAbbr())); ?>"></span>
                                                 <label for="woocommerce_mccp_tokens[<?php echo esc_html($token->getAbbr()); ?>]">
 					                                <input type="checkbox" name="woocommerce_mccp_tokens[<?php echo esc_html($token->getAbbr()); ?>]"
-                                                    id="woocommerce_mccp_tokens[<?php echo esc_html($token->getAbbr()); ?>]">
+                                                    id="woocommerce_mccp_tokens[<?php echo esc_html($token->getAbbr()); ?>]"
+                                                    <?php if ($this->options->getExtra($token->getAbbr()) == 1) : ?>
+                                                    checked="checked"
+                                                    <?php endif; ?>>
                                                     <?php echo $token->getName(); ?>
                                                 </label>
                                             </div>
@@ -267,11 +368,18 @@ class WC_MCCP extends WC_Payment_Gateway
         return ob_get_clean();
     }
 
-    public function validate_networks_field($k, $v) {
-        return array_filter($v);
+    public function validate_networks_field($k, $v)
+    {
+        return $v;
     }
 
-    public function show_invoice_admin_info($order) {
+    public function validate_tokens_field($k, $v)
+    {
+        return ($v == null) ? [] : $v;
+    }
+
+    public function show_invoice_admin_info($order)
+    {
         if (is_admin() && $order->payment_method == 'mccp') {
             echo '<h3>' . __('Payment details', 'mccp') . '</h3>';
             $invoices = WC_MCCP::get_order_invoices($order->get_id());
@@ -298,8 +406,13 @@ class WC_MCCP extends WC_Payment_Gateway
 
     public function do_update()
     {
-        // $version = $this->get_option('version', false);
-        $version = get_option('woocommerce_mccp_settings', false)['version'];
+        return;
+        $settings = get_option('woocommerce_mccp_settings', false); 
+        if (!$settings) {
+            return;
+        }
+
+        $version = $settings['version'];
         $code_version = get_plugin_data(MCCP_MAIN)['Version'];
 
         if(version_compare($code_version, $version, '=')) {
@@ -307,51 +420,45 @@ class WC_MCCP extends WC_Payment_Gateway
         }
 
         // Update to 2.0.0 - Move plugin to SDK
-        $account = get_option('woocommerce_mccp_account');
-        // $this->options = Settings::fromJson($account);
-        $this->options = Options::fromJson($account);
+        $account = get_option('woocommerce_mccp_account', null);
+        $options = ($account) ? Options::fromJson($account) : Options::init()->createAccount();
 
-        // pa( serialize($this->settings['options']));
-        // update_option('woocommerce_mccp_options', $this->settings['options']->toJsonString());
-        // $options = ($account) ? Settings::fromExistingAccount($account->account, $account->{'transfer-key'}) : Settings::init();
-
-        if (version_compare($version, '1.1.0', '>=') && version_compare($version, '1.2.10', '<=')) {
+        if ((version_compare($version, '1.1.0', '>=') && version_compare($version, '1.2.10', '<=')) && $account !== null) {
             // Update table - rename order_id to order
             global $wpdb, $table_prefix;
-            // $table = $table_prefix . 'apirone_mccp';
-            $query = sprintf("ALTER TABLE `%sapirone_mccp` RENAME COLUMN `order_id` to `order`", $table_prefix);
+            $query = "ALTER TABLE `". $table_prefix . "apirone_mccp` RENAME COLUMN `order_id` to `order`";
             $wpdb->query($query);
 
             // // Move options
-            // $options->setMerchant($this->settings['merchant']);
-            // $options->setFactor((float) $this->settings['factor']);
-            // $options->setTimeout((int) $this->settings['timeout']);
-            // $options->setLogo($this->settings['apirone_logo']);
-            // $options->setDebug($this->settings['debug']);
+            $options->setMerchant($settings['merchant']);
+            $options->setFactor((float) $settings['factor']);
+            $options->setTimeout((int) $settings['timeout']);
+            $options->setLogo($settings['apirone_logo']);
+            $options->setDebug($settings['debug']);
 
-            // $options->setExtra('test_customer', $this->settings['test_customer']);
-            // $options->setExtra('processing_fee', $this->settings['processing_fee']);            
+            $options->setExtra('test_customer', $settings['test_customer']);
+            $options->setExtra('processing_fee', $settings['processing_fee']);            
         }
 
         if ($version == false) {
             // Update 1.0.0
-            // $currencies = is_array($this->settings['currencies']) ? $this->settings['currencies'] : [];
-            // foreach ($options->getNetworks() as $network) {
-            //     if (array_key_exists($network->abbr, $currencies)) {
-            //         $network->setAddress($currencies[$network->abbr]['address']);
-            //     }
-            // }
+            $currencies = is_array($settings['currencies']) ? $settings['currencies'] : [];
+            foreach ($options->getNetworks() as $network) {
+                if (array_key_exists($network->abbr, $currencies)) {
+                    $network->setAddress($currencies[$network->abbr]['address']);
+                }
+            }
 
-            // $options->saveCurrencies();
+            $options->saveCurrencies();
         }
 
-        // $settings['enabled'] = $this->settings['enabled'];
+        $settings['enabled'] = $this->settings['enabled'];
         // // $settings['options'] = $options->toJsonString();
         // $settings['secret'] = get_option('woocommerce_mccp_secret');
         // $settings['version'] = $code_version;
 
         unset($this->settings['options']);
-        update_option('woocommerce_mccp_settings', $this->settings);
+        // update_option('woocommerce_mccp_settings', $this->settings);
         // delete_option('woocommerce_mccp_wallets');
         // delete_option('woocommerce_mccp_account');
         // delete_option('woocommerce_mccp_secret');
